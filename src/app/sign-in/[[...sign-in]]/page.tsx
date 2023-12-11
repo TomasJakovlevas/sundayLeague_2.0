f4a175301f3ba51bad2a124bb93d2d33702fd794
TomasJakovlevas <tomasjakovlevas@gmail.com>
2023-12-11 20:48:50 +0200

    style - created custom colors. style - started styling sing in page

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,10 +2,20 @@ import { UserButton, SignIn, SignUp } from '@clerk/nextjs';
 
 export default function Page() {
   return (
-    <main className='flex items-center justify-center md:h-screen'>
-      <div className='relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32'>
-        <SignIn signUpUrl={'http://localhost:3000/sign-up'} />
+    <main className='flex flex-col md:h-screen'>
+      <div className='h-full bg-accent-500'>
+        {/* <div className='h-full bg-gradient-to-b from-primary-500 to-secondary-500 relative'> */}
+
+        <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex gap-[50px] [&_.cl-card]:bg-primary-500 [&_.cl-footerActionLink]:text-accent-500 [&_.cl-formButtonPrimary]:bg-accent-500 hover:[&_.cl-formButtonPrimary]:bg-accent-700 [&_.cl-dividerLine]:bg-accent-500 [&_.cl-dividerText]:text-accent-500 [&_.cl-headerTitle]:text-accent-500 [&_.cl-headerSubtitle]:text-accent-100 hover:[&_.cl-socialButtonsIconButton]:bg-accent-500'>
+          <div className=''>
+            <h1 className='text-[100px] text-center font-black font-test leading-tight'>
+              SUNDAY LEAGUE
+            </h1>
+          </div>
+          <SignIn signUpUrl={'http://localhost:3000/sign-up'} />
+        </div>
       </div>
+      {/* <div className='bg-indigo-200 h-full bg-accent-700'></div> */}
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -13,8 +13,80 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: {
+          50: '#D8EBFD',
+          100: '#B1D6FB',
+          200: '#68B0F8',
+          300: '#1A87F4',
+          400: '#0961B9',
+          500: '#05386B',
+          600: '#042E58',
+          700: '#03213F',
+          800: '#02172C',
+          900: '#010A13',
+          950: '#00050A',
+        },
+        secondary: {
+          50: '#E9F7F4',
+          100: '#D2EFE9',
+          200: '#A5DED3',
+          300: '#79CEBD',
+          400: '#4CBDA7',
+          500: '#379683',
+          600: '#2C7768',
+          700: '#215A4E',
+          800: '#163C34',
+          900: '#0B1E1A',
+          950: '#050F0D',
+        },
+        accent: {
+          50: '#EEFBF4',
+          100: '#DEF8E9',
+          200: '#BCF0D4',
+          300: '#9FEAC1',
+          400: '#7EE2AB',
+          500: '#5CDB95',
+          600: '#2DCD75',
+          700: '#229B58',
+          800: '#166439',
+          900: '#0B321D',
+          950: '#06190E',
+        },
+        background: {
+          50: '#F3FCF6',
+          100: '#EAFAF0',
+          200: '#D2F4DF',
+          300: '#BDEFD0',
+          400: '#A4E9BF',
+          500: '#8EE4AF',
+          600: '#52D685',
+          700: '#2BB560',
+          800: '#1D773F',
+          900: '#0F3E21',
+          950: '#071D0F',
+        },
+        text: {
+          50: '#FDFEFB',
+          100: '#FAFCF7',
+          200: '#F8FBF4',
+          300: '#F4F9EC',
+          400: '#F1F7E8',
+          500: '#EDF5E1',
+          600: '#C3DE9C',
+          700: '#98C653',
+          800: '#688E2F',
+          900: '#324517',
+          950: '#19220B',
+        },
+      },
+      fontFamily: {
+        mirador: ['Mirador'],
+        test: ['Catamaran', 'sans-serif'],
+      },
     },
   },
+
   plugins: [],
-}
-export default config
+};
+export default config;
