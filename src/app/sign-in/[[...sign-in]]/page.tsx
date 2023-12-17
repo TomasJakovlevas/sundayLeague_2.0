70dc171be52f40f69db2340af73d370c970f1f23
TomasJakovlevas <tomasjakovlevas@gmail.com>
2023-12-17 18:14:56 +0200

    style - sing in responsive design

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image';
 
 export default function Page() {
   return (
-    <main className='flex flex-col bg-accent-500 min-h-screen xl:p-20 xl:flex xl:justify-center'>
-      <div className='mx-5 mt-[51px] flex flex-wrap justify-center items-center gap-8  xl:gap-[100px] xl:mt-0 xl:flex-nowrap 2xl:gap-[200px] transition-all'>
+    <main className='flex flex-col bg-accent-500 min-h-screen md:flex md:justify-center xl:p-20'>
+      <div className='mx-5 mt-[51px] flex flex-col justify-center items-center md:flex-row md:mt-0 gap-8 md:gap-[50px] xl:gap-[100px] xl:mt-0 xl:flex-nowrap 2xl:gap-[150px] transition-all'>
         <div className='xl:w-1/2'>
-          <div className='h-[250px] w-[250px] xl:h-[750px] xl:w-[750px] relative  '>
+          <div className='relative h-[250px] w-[250px] md:h-[450px] md:w-[450px] xl:h-[750px] xl:w-[100%]'>
             <Image
               src={'/assets/images/football-battle.png'}
               alt='football-battle'
@@ -16,7 +16,7 @@ export default function Page() {
           </div>
         </div>
         <div className='flex flex-col gap-5 xl:gap-6 xl:w-1/2'>
-          <div className='text-primary-500 xl:w-full xl:w-fit xl:px-[27px]'>
+          <div className='text-primary-500 xl:w-full xl:w-fit'>
             <h1 className='text-3xl mb-8 font-black font-bungy flex flex-wrap gap-2 xl:text-8xl mb-8 xl:flex-col xl:leading-none'>
               <span>SUNDAY</span>
               <span>LEAGUE</span>
@@ -25,7 +25,7 @@ export default function Page() {
               Explore top sports organization app!
             </p>
           </div>
-          <div className='[&_.cl-internal-b3fm6y]:hidden'>
+          <div className='[&_.cl-internal-b3fm6y]:hidden max-w-[430px]'>
             <SignIn afterSignInUrl={'/'} />
           </div>
         </div>
