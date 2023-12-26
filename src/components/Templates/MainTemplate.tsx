6a2f9f40f66c8ba7254bf6362cf1446674fa1116
TomasJakovlevas <tomasjakovlevas@gmail.com>
2023-12-26 15:17:38 +0200

    feat - updated main feed layout

--- src/components/Templates/MainTemplate.tsx
+++ src/components/Templates/MainTemplate.tsx
@@ -5,29 +5,9 @@ export const MainTemplate = ({ children }: { children: React.ReactNode }) => {
     <section className='relative sm:flex sm:gap-8'>
       <InnerNavbar className='sticky top-[64px] sm:hidden' />
 
-      <div className='h-[200vh] bg-[red] p-6 md:px-0'>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus,
-        veniam accusantium deleniti at amet deserunt atque natus quae quod velit
-        consequatur modi culpa, eaque tenetur adipisci neque quidem debitis
-        repellat vero quisquam aspernatur! Consectetur sit numquam minus quod.
-        Magnam laborum cupiditate voluptate incidunt, deserunt hic atque aliquam
-        quaerat iusto iure quia nihil molestias. Natus repudiandae dignissimos
-        eos rem ea quia aperiam eveniet nemo, modi odit at illum quos culpa
-        blanditiis non corporis consequatur, cum deleniti labore commodi facere
-        quasi rerum! Voluptates itaque corrupti necessitatibus similique quidem
-        nam minima deleniti inventore, dignissimos exercitationem! Ipsam
-        voluptatibus tenetur sequi perspiciatis autem quibusdam eius blanditiis
-        officiis fugiat? Pariatur, obcaecati officia tenetur tempore minus
-        repellat aliquam laboriosam laudantium fuga corrupti at magni illum
-        incidunt asperiores beatae cumque commodi? Cumque harum dolorum
-        possimus, nobis odio, deleniti aut doloremque reiciendis est ipsa
-        quisquam praesentium itaque delectus. Placeat aperiam nisi rerum,
-        repellendus enim eveniet totam cum eius amet dolorum, vel aspernatur aut
-        maiores blanditiis debitis ad. Iste nemo, assumenda quis rem distinctio
-        suscipit eveniet illo qui quod aperiam natus sequi accusamus id modi
-        maxime ipsam accusantium facere laborum eum. Quis, nostrum laboriosam
-        beatae, natus, at consectetur ab quam quia pariatur eos modi dolor
-        corrupti sequi ex aliquid ea.
+      <div className='p-6 w-full sm:px-0'>
+        <h1 className='text-xl font-extrabold mb-4'>Title</h1>
+        {children}
       </div>
     </section>
   );
