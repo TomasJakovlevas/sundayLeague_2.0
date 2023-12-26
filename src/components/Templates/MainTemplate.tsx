9a7739378e431c9adaec9befc8bc8b8c215ed194
TomasJakovlevas <tomasjakovlevas@gmail.com>
2023-12-26 17:29:57 +0200

    feat - displaying current page title

--- src/components/Templates/MainTemplate.tsx
+++ src/components/Templates/MainTemplate.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import { LinkType } from '@/types';
 import { InnerNavbar } from '../InnerNavbar/InnerNavbar';
+import { usePathname } from 'next/navigation';
 
 type MainTemplateProps = {
   children: React.ReactNode;
@@ -7,6 +10,10 @@ type MainTemplateProps = {
 };
 
 export const MainTemplate = ({ children, menuItems }: MainTemplateProps) => {
+  const pathname = usePathname();
+
+  const activeRoute = menuItems.find((item) => item.route === pathname);
+
   return (
     <section className='relative sm:flex sm:gap-8'>
       <InnerNavbar
@@ -14,8 +21,8 @@ export const MainTemplate = ({ children, menuItems }: MainTemplateProps) => {
         menuItems={menuItems}
       />
 
-      <div className='p-6 w-full sm:px-0'>
-        <h1 className='text-xl font-extrabold mb-4'>Title</h1>
+      <div className='p-6 w-full sm:px-0 '>
+        <h1 className='text-3xl font-extrabold mb-4'>{activeRoute?.title}</h1>
         {children}
       </div>
     </section>
