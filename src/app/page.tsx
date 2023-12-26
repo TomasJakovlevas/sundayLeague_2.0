1dfc23db20a92398ce64f54e1cf532444379fa4a
TomasJakovlevas <tomasjakovlevas@gmail.com>
2023-12-26 18:59:19 +0200

    feat - wip: creating game card and filter layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,16 @@ import { mainPageRoutes } from '@/lib/routes';
 export default function Home() {
   return (
     <Template>
-      <MainTemplate menuItems={mainPageRoutes}>va</MainTemplate>
+      <MainTemplate menuItems={mainPageRoutes}>
+        <div className='border mb-3'>filter? selects?</div>
+        <div className='border flex flex-col gap-5'>
+          {Array.from({ length: 10 }).map((_, index) => (
+            <div key={index} className='border w-fit px-2 py-3'>
+              card
+            </div>
+          ))}
+        </div>
+      </MainTemplate>
     </Template>
   );
 }
--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -12,14 +12,10 @@ export const mainPageRoutes: LinkType[] = [
     title: 'Home',
     route: '/',
   },
-  {
-    title: 'All events',
-    route: '/all-events',
-  },
-  {
-    title: 'Upcoming games',
-    route: '/upcoming-games',
-  },
+  //   {
+  //     title: 'Upcoming games',
+  //     route: '/upcoming-games',
+  //   },
   {
     title: 'Groups',
     route: '/groups',
