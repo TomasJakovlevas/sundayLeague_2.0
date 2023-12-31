9023fe58b10af9a29b754d6bb6ee6640aea47eb1
TomasJakovlevas <tomasjakovlevas@gmail.com>
2023-12-31 15:36:18 +0200

    style - updated Game quick preview component

--- src/components/GameCard/GameCardPreview.tsx
+++ src/components/GameCard/GameCardPreview.tsx
@@ -5,11 +5,11 @@ import { GameCardList } from './GameCardList';
 export const GameCardPreview = () => {
   return (
     <div>
-      <div className='flex items-start gap-3 flex border'>
+      <div className='flex items-start gap-3 flex py-3'>
         <GameCardHeader />
       </div>
 
-      <div className='flex flex-col gap-3 border'>
+      <div className='flex flex-col gap-3 py-3'>
         <GameCardBody />
         <GameCardList />
       </div>
