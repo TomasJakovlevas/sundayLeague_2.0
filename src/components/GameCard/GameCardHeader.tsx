bd26fa40c4e28222f65d1b722c5fd77861f912e3
TomasJakovlevas <tomasjakovlevas@gmail.com>
2024-01-02 17:42:07 +0200

    style - updated game card mobile view

--- src/components/GameCard/GameCardHeader.tsx
+++ src/components/GameCard/GameCardHeader.tsx
@@ -5,13 +5,12 @@ export const GameCardHeader = () => {
     <>
       <Image
         alt='nextui logo'
-        height={40}
         radius='sm'
         src='https://avatars.githubusercontent.com/u/86160567?s=200&v=4'
-        width={40}
+        className='h-[30px] w-[30px] sm:h-[40px] sm:w-[40px]'
       />
       <div className='flex justify-between gap-2 flex-1'>
-        <span className='text-md'>Garbanelius</span>
+        <span className='text-sm sm:text-base'>Garbanelius</span>
       </div>
     </>
   );
