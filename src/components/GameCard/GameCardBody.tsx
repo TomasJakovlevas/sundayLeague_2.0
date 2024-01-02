b60cbc801c3286946ca05ddfd9f10e8c268af23e
TomasJakovlevas <tomasjakovlevas@gmail.com>
2024-01-02 17:47:58 +0200

    style - updated gameCardBody mobile view

--- src/components/GameCard/GameCardBody.tsx
+++ src/components/GameCard/GameCardBody.tsx
@@ -3,17 +3,17 @@ import { Button } from '@nextui-org/react';
 export const GameCardBody = () => {
   return (
     <>
-      <div className='flex flex-row gap-3 items-center whitespace-nowrap'>
-        <div className='flex items-center gap-3'>
+      <div className='flex flex-row gap-2 items-center whitespace-nowrap sm:gap-3'>
+        <div className='flex items-center gap-2 sm:gap-3'>
           <div className='w-[20px] h-[20px] border border-[red]'>
             {
               // TODO: place icon
             }
           </div>
-          <span>Place</span>
+          <span className='text-sm sm:text-base'>Place</span>
         </div>
         <div>
-          <div className='flex items-center gap-3'>
+          <div className='flex items-center gap-2 sm:gap-3'>
             <div className='w-[20px] h-[20px] border border-[red]'>
               {
                 // TODO: Type icon
@@ -22,7 +22,7 @@ export const GameCardBody = () => {
             <span>Type</span>
           </div>
         </div>
-        <div className='flex items-center gap-3'>
+        <div className='flex items-center gap-2 sm:gap-3'>
           <div className='w-[20px] h-[20px] border border-[red]'>
             {
               // TODO: calendar icon
@@ -31,13 +31,13 @@ export const GameCardBody = () => {
           <span>Tomorow</span>
         </div>
       </div>
-      <div className='flex items-center justify-end gap-3'>
+      <div className='flex items-center justify-end gap-2 sm:gap-3'>
         <div className='w-[20px] h-[20px] border border-[red]'>
           {
             // TODO: players icon
           }
         </div>
-        <span className='text-lg font-semibold'>3/14</span>
+        <span className='text-base font-semibold sm:text-lg'>3/14</span>
         {/* <Button size='sm' variant='flat' color='primary'>
           Join
         </Button> */}
