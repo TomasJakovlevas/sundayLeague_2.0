90c51bf8682686d53fff93fff4a16c6b39a4721c
TomasJakovlevas <tomasjakovlevas@gmail.com>
2024-01-03 20:42:51 +0200

    style - updated loading component mobile view

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,11 +3,11 @@ import SundayLeaguLogo from '@/assets/icons/sundayLeague.svg';
 
 export default function Loading() {
   return (
-    <main className='h-screen flex flex-col items-center justify-center gap-10'>
-      <h2 className='text-[100px]'>
+    <main className='h-screen flex flex-col items-center justify-center gap-10 p-3'>
+      <h2 className='text-[60px] sm:text-[100px]'>
         <SundayLeaguLogo />
       </h2>
-      <div className='w-full flex justify-center'>
+      <div className='w-1/2 flex justify-center'>
         <Progress
           size='sm'
           isIndeterminate
