0bc4446ba3e55d08230298dd36b94c5295d708d5
TomasJakovlevas <tomasjakovlevas@gmail.com>
2024-01-10 19:19:53 +0200

    feat - game card added link to the game page

--- src/components/GameCard/GameCardHeader.tsx
+++ src/components/GameCard/GameCardHeader.tsx
@@ -1,4 +1,5 @@
 import { Image } from '@nextui-org/react';
+import Link from 'next/link';
 
 export const GameCardHeader = () => {
   return (
@@ -9,8 +10,17 @@ export const GameCardHeader = () => {
         src='https://avatars.githubusercontent.com/u/86160567?s=200&v=4'
         className='h-[30px] w-[30px] sm:h-[40px] sm:w-[40px]'
       />
-      <div className='flex justify-between gap-2 flex-1'>
-        <span className='text-sm sm:text-base'>Garbanelius</span>
+      <div className='flex justify-between flex-1'>
+        <div className='flex items-center gap-2'>
+          <span className='text-sm sm:text-base'>Garbanelius</span>
+          <Link href={'/games/69'}>
+            <div className='w-[20px] h-[20px] border border-[red] hover:cursor-pointer'>
+              {
+                // TODO: link
+              }
+            </div>
+          </Link>
+        </div>
       </div>
     </>
   );
