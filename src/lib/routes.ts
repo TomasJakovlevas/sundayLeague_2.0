d976a7e3100aa45633f7e1f95aaac94f3f219a24
TomasJakovlevas <tomasjakovlevas@gmail.com>
2024-01-11 21:16:46 +0200

    fix - hidded group routes, and fixed my game route

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,10 +1,10 @@
 import { LinkType } from '@/types';
 
 export const mainNavRoutes: LinkType[] = [
-  {
-    title: 'Groups',
-    route: '/groups',
-  },
+  // {
+  //   title: 'Groups',
+  //   route: '/groups',
+  // },
   {
     title: 'My games',
     route: '/my-games',
@@ -12,23 +12,25 @@ export const mainNavRoutes: LinkType[] = [
 ];
 
 export const mainPageRoutes: LinkType[] = [
-  {
-    title: 'Groups',
-    route: '/groups',
-  },
-  {
-    title: 'My games',
-    route: '/my-games',
-  },
+  // {
+  //   title: 'Groups',
+  //   route: '/groups',
+  // },
+  // {
+  //   title: 'My games',
+  //   route: '/my-games',
+  // },
 ];
 
 export const myGamesRoutes: LinkType[] = [
   {
     title: 'Games',
-    route: '/games',
+    route: '/my-games/games',
   },
   {
     title: 'Create game',
     route: '/my-games/create-game',
   },
 ];
+
+export const groupRoutes: LinkType[] = [];
