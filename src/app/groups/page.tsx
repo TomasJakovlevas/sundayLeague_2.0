1f02ccaebe5759004e4908b42e5f8301ba9e4685
TomasJakovlevas <tomasjakovlevas@gmail.com>
2024-01-11 21:19:25 +0200

    fix - changed menu items for group page

--- src/app/groups/page.tsx
+++ src/app/groups/page.tsx
@@ -1,11 +1,11 @@
 import { MainTemplate } from '@/components/Templates/MainTemplate';
 import { Template } from '@/components/Templates/Template';
-import { mainPageRoutes } from '@/lib/routes';
+import { groupRoutes } from '@/lib/routes';
 
 const page = () => {
   return (
     <Template>
-      <MainTemplate menuItems={mainPageRoutes}>Groups</MainTemplate>
+      <MainTemplate menuItems={groupRoutes}>Groups</MainTemplate>
     </Template>
   );
 };
