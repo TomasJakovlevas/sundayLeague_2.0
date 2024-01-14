ec1c6fc38f63c0af90d2ac8f4e6c77e368486280
TomasJakovlevas <tomasjakovlevas@gmail.com>
2024-01-14 14:03:07 +0200

    feat - updated Select component to accept ref

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,8 +1,13 @@
 'use client';
 
-import { Select as S, SelectItem, Selection } from '@nextui-org/react';
+import {
+  Select as S,
+  SelectItem,
+  Selection,
+  SkeletonProps,
+} from '@nextui-org/react';
 import { getPreset } from './SelectPresets';
-import { useState } from 'react';
+import { forwardRef, useState } from 'react';
 
 type SelectProps = {
   label: string;
@@ -10,33 +15,39 @@ type SelectProps = {
   type: string;
 };
 
-export const Select = ({ label, value, type }: SelectProps) => {
-  const preset = getPreset(type);
+export const Select = forwardRef<HTMLSelectElement, SelectProps>(
+  ({ label, value, type, ...rest }, ref) => {
+    const preset = getPreset(type);
 
-  const [selectValue, setSelectValue] = useState<Selection>(new Set([]));
+    const [selectValue, setSelectValue] = useState<Selection>(new Set([]));
 
-  const variant = Array.from(selectValue).length ? 'flat' : 'bordered';
+    const variant = Array.from(selectValue).length ? 'flat' : 'bordered';
 
-  return (
-    <S
-      items={preset?.items}
-      label={label}
-      className='min-w-[120px] w-[120px]'
-      classNames={{
-        trigger: 'data-[hover]:hover:border-primary-500 h-full',
-        mainWrapper: 'h-full',
-      }}
-      selectedKeys={selectValue}
-      variant={variant}
-      color={'primary'}
-      radius='md'
-      onSelectionChange={setSelectValue}
-    >
-      {preset?.items.map((item) => (
-        <SelectItem key={item.value} value={item.value} color={'primary'}>
-          {item.label}
-        </SelectItem>
-      ))}
-    </S>
-  );
-};
+    return (
+      <S
+        {...rest}
+        ref={ref}
+        items={preset?.items}
+        label={label}
+        className='min-w-[120px] w-[120px]'
+        classNames={{
+          trigger: 'data-[hover]:hover:border-primary-500 h-full',
+          mainWrapper: 'h-full',
+        }}
+        selectedKeys={selectValue}
+        variant={variant}
+        color={'primary'}
+        radius='md'
+        onSelectionChange={setSelectValue}
+      >
+        {preset?.items.map((item) => (
+          <SelectItem key={item.value} value={item.value} color={'primary'}>
+            {item.label}
+          </SelectItem>
+        ))}
+      </S>
+    );
+  }
+);
+
+Select.displayName = 'Select';
