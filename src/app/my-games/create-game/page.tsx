ea8eea8669bbe5e16458620d197b22c68ff1da20
TomasJakovlevas <tomasjakovlevas@gmail.com>
2024-01-14 14:03:26 +0200

    feat - updated Input component to accept ref

--- src/app/my-games/create-game/page.tsx
+++ src/app/my-games/create-game/page.tsx
@@ -1,4 +1,5 @@
-import DateRanges from '@/components/DateRange/DateRange';
+'use client';
+
 import { Select } from '@/components/Select/Select';
 import { MainTemplate } from '@/components/Templates/MainTemplate';
 import { Template } from '@/components/Templates/Template';
@@ -6,12 +7,33 @@ import { myGamesRoutes } from '@/lib/routes';
 import { Button } from '@nextui-org/react';
 import { Input } from '@/components/Input/Input';
 import { Calendar } from '@/components/Calendar/Calendar';
+import { SubmitHandler, useForm, Controller } from 'react-hook-form';
+import { useEffect, useState } from 'react';
+
+type Inputs = {};
 
 export default function Page() {
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit: SubmitHandler<Inputs> = (data: any) => {
+    console.log(data);
+  };
+
+  const [date, setDate] = useState<any>(new Date());
+
+  useEffect(() => {
+    register('date', { value: date });
+  }, [register, date]);
+
   return (
     <Template>
       <MainTemplate menuItems={myGamesRoutes}>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className='border border-primary-200 flex flex-col items-center p-6 gap-6 rounded-small w-1/2 '>
             <div className='flex w-full'>
               <div className='flex h-[50px]'>
@@ -20,47 +42,90 @@ export default function Page() {
             </div>
             <div className='flex gap-5 h-[50px] w-full'>
               <div className='flex'>
-                <Select type='category' label='Category' />
+                <Controller
+                  name='category'
+                  control={control}
+                  render={({ field }) => (
+                    <Select
+                      {...field}
+                      {...register('category')}
+                      type='category'
+                      label='Category'
+                    />
+                  )}
+                />
               </div>
               <div className='flex flex-1'>
-                <Input
-                  type='number'
-                  labelPlacement='inside'
-                  label='Number of players'
-                  variant='bordered'
-                  isClearable
+                <Controller
+                  name='number_of_players'
+                  control={control}
+                  render={({ field }) => (
+                    <Input
+                      {...field}
+                      {...register('number_of_players')}
+                      type='number'
+                      labelPlacement='inside'
+                      label='Number of players'
+                      variant='bordered'
+                      isClearable
+                    />
+                  )}
                 />
               </div>
             </div>
             <div className='flex gap-5 h-[50px] w-full'>
               <div className='flex flex-1'>
-                <Input
-                  type='text'
-                  labelPlacement='inside'
-                  label='Place'
-                  isClearable
+                <Controller
+                  name='place'
+                  control={control}
+                  render={({ field }) => (
+                    <Input
+                      {...field}
+                      {...register('place')}
+                      type='text'
+                      labelPlacement='inside'
+                      label='Place'
+                      isClearable
+                    />
+                  )}
                 />
               </div>
               <div className='flex flex-1'>
-                <Input
-                  type='number'
-                  labelPlacement='inside'
-                  label='Price'
-                  endContent='€'
-                  isClearable
+                <Controller
+                  name='price'
+                  control={control}
+                  render={({ field }) => (
+                    <Input
+                      {...field}
+                      {...register('price')}
+                      type='number'
+                      labelPlacement='inside'
+                      label='Price'
+                      endContent='€'
+                      isClearable
+                    />
+                  )}
                 />
               </div>
             </div>
             <div className='flex w-full'>
-              <Input
-                type='text'
-                labelPlacement='inside'
-                label='Note'
-                isClearable
+              <Controller
+                name='note'
+                control={control}
+                render={({ field }) => (
+                  <Input
+                    {...field}
+                    {...register('note')}
+                    type='text'
+                    labelPlacement='inside'
+                    label='Note'
+                    isClearable
+                  />
+                )}
               />
             </div>
             <div className='flex justify-end w-full'>
-              <Button variant={'solid'} color='primary'>
+              <Button variant={'solid'} color='primary' type='submit'>
                 Create
               </Button>
             </div>
@@ -69,25 +134,4 @@ export default function Page() {
       </MainTemplate>
     </Template>
   );
-}
-
-{
-  /* <div className='flex gap-2'>
-              <span>Select date </span>
-              <DateRanges />
-            </div>
-            <div>
-              <Select type='category' label='Category' />
-            </div>
-            <div>
-              <Input
-                type='text'
-                labelPlacement='inside'
-                label='How many people?'
-              />
-            </div>
-            <div>Players: Input</div>
-            <div>Price: Input</div>
-            <div>Price: Input</div>
-            <div>Price: Input</div> */
 }
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,21 +1,27 @@
 'use client';
+
 import { Input as Inp, InputProps } from '@nextui-org/react';
-import { useState } from 'react';
+import { forwardRef, useState } from 'react';
 
-export const Input = ({ ...rest }: InputProps) => {
-  const [value, setValue] = useState('');
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ ...rest }, ref) => {
+    const [value, setValue] = useState('');
 
-  const variant = value !== '' ? 'flat' : 'bordered';
+    const variant = value !== '' ? 'flat' : 'bordered';
 
-  return (
-    <Inp
-      {...rest}
-      variant={variant}
-      value={value}
-      onValueChange={setValue}
-      color='primary'
-      // isInvalid={true}
-      // errorMessage='Input is invalid'
-    />
-  );
-};
+    return (
+      <Inp
+        ref={ref}
+        {...rest}
+        variant={variant}
+        value={value}
+        onValueChange={setValue}
+        color='primary'
+        // isInvalid={true}
+        // errorMessage='Input is invalid'
+      />
+    );
+  }
+);
+
+Input.displayName = 'Input';
